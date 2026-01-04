da16f7fc35fb9dbab3891d75c300446f4c2a0214
redcommxg <redcommxg@aol.com>
2026-01-04 22:02:37 +0000

    File changes

--- src/pages/Blueprint.jsx
+++ src/pages/Blueprint.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import AuroraBackground from '../components/omni/AuroraBackground';
 import BlueprintSection from '../components/omni/BlueprintSection';
 import PerformanceMetrics from '../components/omni/PerformanceMetrics';
+import NotificationSystem from '../components/omni/NotificationSystem';
 
 export default function Blueprint() {
   return (
     <AuroraBackground className="min-h-screen">
       <PerformanceMetrics />
+      <NotificationSystem />
       <div className="pt-24 pb-16">
         <BlueprintSection />
       </div>
