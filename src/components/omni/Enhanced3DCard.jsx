963a37fc2ea5bbcc5bae19de990ebe32680525a0
x3demonx <x3demonx@yahoo.com>
2026-01-09 20:30:34 +0000

    File changes

--- src/components/omni/Enhanced3DCard.jsx
+++ src/components/omni/Enhanced3DCard.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Float, Sphere, TorusKnot, Tetrahedron } from '@react-three/drei';
 import { motion } from 'framer-motion';
+import * as THREE from 'three';
 
 function Interactive3DObject({ color, shape = 'sphere' }) {
   const meshRef = useRef();
