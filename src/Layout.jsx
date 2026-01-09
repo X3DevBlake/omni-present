eac0cfc039650ab63c86ae96dd930e4131c17432
x3demonx <x3demonx@yahoo.com>
2026-01-09 21:00:52 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,6 +3,7 @@ import EnhancedMainNav from './components/navigation/EnhancedMainNav';
 import OmniAssistant from './components/ai/OmniAssistant';
 import { GamificationProvider } from './components/gamification/GamificationContext';
 import { PersonalizationProvider } from './components/personalization/PersonalizationContext';
+import OmniPresentLogo from './components/omni/OmniPresentLogo';
 
 export default function Layout({ children }) {
   return (
